d363febbc68f72856a9972ff16d5a110da077b9a
Tung Pham <tung.42@gmail.com>
2025-02-25 17:30:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import TopicPicker from "./components/TopicPicker";
 import { Container } from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
     <Container className="my-5 col-xl-8 col-lg-8 col-md-8 col-sm-10 col-12">
       <TopicPicker />
     </Container>
   );
-}
+};
 
 export default App;
