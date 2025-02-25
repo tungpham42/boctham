71455632fc53081996f40e96a6545fb9a08fc3a8
Tung Pham <tung.42@gmail.com>
2025-02-25 15:43:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container className="my-5 col-md-6 col-sm-8">
+    <Container className="my-5 col-xl-6 col-lg-6 col-md-8 col-sm-10">
       <TopicPicker />
     </Container>
   );
